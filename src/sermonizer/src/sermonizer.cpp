acad0009032538ab766a76ceb78231715a01f96f
david-beaudette <beaudette.david@gmail.com>
2021-04-05 22:11:31 -0400

    Configured VSCode for compilation and debug of executable.

---
+++ src/sermonizer/src/sermonizer.cpp
@@ -0,0 +1,13 @@
+// Copyright 2021 David Beaudette
+
+#include <cstdio>
+#include <sermonizer/serialib.h>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world sermonizer package\n");
+  return 0;
+}
