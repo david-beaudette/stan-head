ef28c9ba2523ccb931346bbc084c52f76463d408
david-beaudette <beaudette.david@gmail.com>
2021-08-18 07:46:36 -0400

    Removed setting ROS env. variables; now works.

--- src/stan_common/scripts/rpi_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export ROS_HOSTNAME=$(hostname).local
-export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
--- src/stan_common/scripts/rpi_stan.sh
+++ src/stan_common/scripts/rpi_stan.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 source /home/ubuntu/ros1_ws/install/setup.sh
-source /home/ubuntu/ros1_ws/install/share/stan_common/scripts/rpi_env.sh
 
 roslaunch stan_common stan_head_rpi.launch &
 PID=$!
