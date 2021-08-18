15950312ca88b16954a6f8bc712129196ae3c658
david-beaudette <beaudette.david@gmail.com>
2021-08-18 07:03:42 -0400

    Attempt to fix services.

--- src/stan_common/scripts/rpi_stan.sh
+++ src/stan_common/scripts/rpi_stan.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
 source /home/ubuntu/ros1_ws/install/setup.sh
-roslaunch stan_common stan_head_rpi.launch
+source /home/ubuntu/ros1_ws/install/share/stan_common/scripts/rpi_env.sh
+
+roslaunch stan_common stan_head_rpi.launch &
+PID=$!
+wait “$PID”
